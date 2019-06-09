c5e2d52047ec3e7936ca6897d195a2ef791c0f9d
Preston Elliott <preston.elliott24@gmail.com>
2019-06-09 15:22:39 -0400

    second check in

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function handleClickEvents(e) {
     if(e.target.className === "dog_span") dogDetails(e.target)
-    // else if (e.target.className === "")
+    else if(e.target.id === "dogStatus_btn") dogStatus(e.target)
 }
 
 function getDogs() { 
@@ -13,7 +13,7 @@ function getDogs() {
     .then(res => res.json())
     .then(res => 
         {res.forEach(dog => displayDog(dog))
-        })
+    })
 } 
 
 function displayDog(dog) {
@@ -29,5 +29,18 @@ function dogDetails(dog_span) {
     document.getElementById("dog-info").innerHTML = 
     `<img src=${dog_span.dataset.image}>
         <h2>${dog_span.dataset.name}</h2>
-    <button>Good Dog!</button>`
+    <button id="dogStatus_btn" data-dog-id="${dog_span.id}">${dog_span.dataset.isgooddog === "true" ? "Good Dog!" : "Bad Dog!"}</button>`
+
+}
+
+function dogStatus(button) {
+    const dog_span = document.getElementById(button.dataset.dogId)
+    dog_span.dataset.isgooddog = dog_span.dataset.isgooddog === "false"
+    button.innerText = button.innerText === "Good Dog!" ? "Bad Dog!" : "Good Dog!"
+
+    fetch(`http://localhost:3000/pups/${button.dataset.dogId}`,{
+        method: "PATCH",
+        headers: { "Content-Type":"application/json" },
+        body: JSON.stringify({isGoodDog: dog_span.dataset.isgooddog})
+    })
 }
